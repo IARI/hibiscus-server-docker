996eeb2aa20c311374b72530f22362a601b7b451
IARI <julian.jarecki@gmail.com>
2023-01-25 19:28:40 +0100

    updated node script to scrape and write release version

--- get-version-from-changelog.js
+++ get-version-from-changelog.js
@@ -1,5 +1,6 @@
 import { DOMParser } from 'xmldom'
 import xpath from 'xpath'
+import fs from 'fs'
 
 (async function(){
     const url = "https://www.willuhn.de/products/hibiscus-server/changelog.php"
@@ -11,27 +12,15 @@ import xpath from 'xpath'
         errorHandler: {warning: warnCallback, error: warnCallback,fatalError: warnCallback}
     });
     const xmlDoc = parser.parseFromString(htmlStr, "text/html");
-
-    
-    const done = []
-    function traverse(node) {
-        done.push(node)
-        const len = node.childNodes?.length ?? 0;
-        console.log(node.nodeType + " " + node.nodeName+" with "+len+" children.")
-        for (let i = 0; i < len; i++) {
-            const child = node.childNodes.item(i);
-            const isnew = done.indexOf(child)<0
-            if (isnew) {
-                traverse(child)
-            } else {
-                console.log(child.nodeType + " " + child.nodeName+" (circular)")
-            }
-        }
-    }
-    //traverse(xmlDoc)
     const path = xpath.parse('//pre/text()');
     const data = path.select({node: xmlDoc, isHtml: true})
     console.log("got "+data.length+" results")
     const [releaseDate,,version] = data[0].data.split("\n")[0].split(" ");
     console.log({releaseDate, version})
+    fs.writeFile('release-version', version, err=>{
+        if (err) {
+            console.error("could not write release-version");
+            console.error(err);
+        }
+    })
 })()
